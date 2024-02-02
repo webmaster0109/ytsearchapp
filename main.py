7dd6cf27d43ccc63686e8c47eba1dfc1f21e90ea
webmaster0109 <itdepartmentvfo@gmail.com>
2024-02-02 10:41:33 +0530

    app

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ def search_youtube_videos(query, max_results=100):
 
 def main():
 
-    html_metadata = f"""
-        <head>
-            <title>YouTube Video Search App</title>
-            <meta name="description" content="YouTube Video Search App">
-        </head>
-    """
-    st.markdown(html_metadata, unsafe_allow_html=True)
-
     st.title(f":rainbow[YouTube Video Search App]")
 
     if 'watchlist' not in st.session_state:
