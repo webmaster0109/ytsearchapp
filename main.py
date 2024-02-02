8966a5895a0ee4e05b647508a1531dc9e0347bcb
webmaster0109 <itdepartmentvfo@gmail.com>
2024-02-02 10:34:29 +0530

    app

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
             # time.sleep(1)
             eclapsed_time_second = end_time - start_time
             st.markdown(
-                f"<p style='font-size:16px;color:lightgrey;'>About {total_results} results ({eclapsed_time_second:.2f} seconds)</p>", 
+                f"<p style='font-size:16px;color:grey;'>About {total_results} results ({eclapsed_time_second:.2f} seconds)</p>", 
                 unsafe_allow_html=True
             )
 
